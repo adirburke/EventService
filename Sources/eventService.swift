3ad2c3779b53da28e72681897e965716251146a0
Adir Burke on Laptop <adir@hanave.com>
2020-08-27 16:13:50 +1000

    Added in EventService and alot mroe logging

--- Sources/eventService.swift
+++ Sources/eventService.swift
@@ -4,21 +4,32 @@ import Zara_Logger
 
 
 
-public final class EventManager {
+public  struct EventManager {
     let service : String
+    
+//    private let logger : LogService
     
     public init(service name : String) {
         self.service = name
     }
     
     public func logError(eventId : UUID,  message : String) {
-        
+        let logger = LogService(name: eventId.uuidString, withStart: false)
+        logger.logMessage("Error:: > \(service) -> \(message)", console: false)
     }
     
     public func logEvent<T>(eventId : UUID, message : String = "", item : T) where T : Encodable {
+        let logger = LogService(name: eventId.uuidString)
+        let encoder = JSONEncoder()
+        if let data = try? encoder.encode(item), let parmsString = String(data: data, encoding:  .utf8) {
+            logger.logMessage("\(service) ->\(message) - \(parmsString)", console: false)
+        } else {
         
+            logger.logMessage("\(service) -> \(message)", console: false)
+        }
     }
     public func logEvent(eventId : UUID, message : String = "") {
-        
+        let logger = LogService(name: eventId.uuidString, withStart: false)
+        logger.logMessage("\(service) -> \(message)", console: false)
     }
 }
