d77b66b6fb9d799bebf0d633aba9d397425e1d8b
Adir Burke on Laptop <adir@hanave.com>
2020-09-02 00:03:57 +1000

    Rewriting to use new RMSAPI

--- Sources/eventService.swift
+++ Sources/eventService.swift
@@ -28,6 +28,19 @@ public  struct EventManager {
             logger.logMessage("\(service) -> \(message)", console: false)
         }
     }
+    public func logEvent<T>(eventId : UUID, message : String = "", items: T...) where T : Encodable {
+        let logger = LogService(name: eventId.uuidString)
+        let encoder = JSONEncoder()
+        for i in items {
+            if let data = try? encoder.encode(i), let parmsString = String(data: data, encoding:  .utf8) {
+                logger.logMessage("\(service) ->\(message) - \(parmsString)", console: false)
+            } else {
+            
+                logger.logMessage("\(service) -> \(message)", console: false)
+            }
+        }
+    }
+    
     public func logEvent(eventId : UUID, message : String = "") {
         let logger = LogService(name: eventId.uuidString, withStart: false)
         logger.logMessage("\(service) -> \(message)", console: false)
