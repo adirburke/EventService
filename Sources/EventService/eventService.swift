692c32305411512e8c20f3384d8e593500c444a7
Adir Burke on Laptop <adir@hanave.com>
2020-10-02 12:51:35 +1000

    Fixing moduleS

---
+++ Sources/EventService/eventService.swift
@@ -0,0 +1,77 @@
+import Foundation
+import Zara_Logger
+
+
+
+
+public  struct EventManager {
+    let service : String
+    let eventId : UUID
+//    private let logger : LogService
+    
+    public init(service name : String, eventId : UUID = UUID()) {
+        self.service = name
+        self.eventId = eventId
+    }
+    
+    
+    
+    public func logError(eventId : UUID? = nil,  message : String) {
+        let logger : LogService
+        if let eventId = eventId {
+            logger = LogService(name: eventId.uuidString, withStart: false)
+        } else {
+            logger = LogService(name: self.service, withStart: false)
+        }
+       
+        logger.logMessage("Error:: > \(service) -> \(message)", console: false)
+    }
+    
+    public func logEvent<T>(eventId : UUID? = nil, message : String = "", item : T) where T : Encodable {
+        let eventId = eventId ?? self.eventId
+        let logger = LogService(name: eventId.uuidString, withStart: false)
+        let encoder = JSONEncoder()
+        if let data = try? encoder.encode(item), let parmsString = String(data: data, encoding:  .utf8) {
+            logger.logMessage("\(service) ->\(message) - \(parmsString)", console: false)
+        } else {
+        
+            logger.logMessage("\(service) -> \(message)", console: false)
+        }
+    }
+    public func logEvent<T>(eventId : UUID? = nil, message : String = "", items: T...) where T : Encodable {
+        let eventId = eventId ?? self.eventId
+        let logger = LogService(name: eventId.uuidString, withStart: false)
+        let encoder = JSONEncoder()
+        for i in items {
+            if let data = try? encoder.encode(i), let parmsString = String(data: data, encoding:  .utf8) {
+                logger.logMessage("\(service) ->\(message) - \(parmsString)", console: false)
+            } else {
+            
+                logger.logMessage("\(service) -> \(message)", console: false)
+            }
+        }
+    }
+    
+    public func logEvent(eventId : UUID? = nil, message : String = "") {
+        let eventId = eventId ?? self.eventId
+        let logger = LogService(name: eventId.uuidString, withStart: false)
+        logger.logMessage("\(service) -> \(message)", console: false)
+    }
+    
+    public func logMain<T>(message : String = "", item: T...) where T : Encodable {
+//        let eventId = self.eventId
+        let logger = LogService(name: self.service, withStart: false)
+        let encoder = JSONEncoder()
+        if let data = try? encoder.encode(item), let parmsString = String(data: data, encoding:  .utf8) {
+            logger.logMessage("\(service) ->\(message) - \(parmsString)", console: false)
+        } else {
+            logger.logMessage("\(service) -> \(message)", console: false)
+        }
+    }
+    public func logMain(message : String) {
+//        let eventId =  self.eventId
+        let logger = LogService(name: self.service, withStart: false)
+        logger.logMessage("\(service) -> \(message)", console: false)
+    }
+    
+}
