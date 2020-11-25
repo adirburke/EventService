f9f4034378caed69ea0058023248757c34f22b00
Adir Burke on Laptop <adir@hanave.com>
2020-11-25 12:58:16 +1100

    Fixed some logging

--- Sources/EventService/eventService.swift
+++ Sources/EventService/eventService.swift
@@ -7,11 +7,14 @@ import Zara_Logger
 public  struct EventManager {
     let service : String
     let eventId : UUID
+    let logger : LogService
+    
 //    private let logger : LogService
     
     public init(service name : String, eventId : UUID = UUID()) {
         self.service = name
         self.eventId = eventId
+        self.logger = LogService(name: name, withStart: false)
     }
     
     
@@ -28,39 +31,47 @@ public  struct EventManager {
     }
     
     public func logEvent<T>(eventId : UUID? = nil, message : String = "", item : T) where T : Encodable {
-        let eventId = eventId ?? self.eventId
-        let logger = LogService(name: eventId.uuidString, withStart: false)
-        let encoder = JSONEncoder()
-        if let data = try? encoder.encode(item), let parmsString = String(data: data, encoding:  .utf8) {
-            logger.logMessage("\(service) ->\(message) - \(parmsString)", console: false)
+        if let eventId  = eventId {
+            let logger = LogService(name: eventId.uuidString, withStart: false)
+            let encoder = JSONEncoder()
+            if let data = try? encoder.encode(item), let parmsString = String(data: data, encoding:  .utf8) {
+                logger.logMessage("\(service) ->\(message) - \(parmsString)", console: false)
+            } else {
+                
+                logger.logMessage("\(service) -> \(message)", console: false)
+            }
         } else {
-        
-            logger.logMessage("\(service) -> \(message)", console: false)
+            logMain(message: message, item: item)
         }
     }
     public func logEvent<T>(eventId : UUID? = nil, message : String = "", items: T...) where T : Encodable {
-        let eventId = eventId ?? self.eventId
-        let logger = LogService(name: eventId.uuidString, withStart: false)
-        let encoder = JSONEncoder()
-        for i in items {
-            if let data = try? encoder.encode(i), let parmsString = String(data: data, encoding:  .utf8) {
-                logger.logMessage("\(service) ->\(message) - \(parmsString)", console: false)
-            } else {
-            
-                logger.logMessage("\(service) -> \(message)", console: false)
+        if let eventId = eventId {
+            let logger = LogService(name: eventId.uuidString, withStart: false)
+            let encoder = JSONEncoder()
+            for i in items {
+                if let data = try? encoder.encode(i), let parmsString = String(data: data, encoding:  .utf8) {
+                    logger.logMessage("\(service) ->\(message) - \(parmsString)", console: false)
+                } else {
+                    
+                    logger.logMessage("\(service) -> \(message)", console: false)
+                }
             }
+        } else {
+            logMain(message: message, item: items)
         }
     }
     
     public func logEvent(eventId : UUID? = nil, message : String = "") {
-        let eventId = eventId ?? self.eventId
+        if let eventId = eventId {
         let logger = LogService(name: eventId.uuidString, withStart: false)
         logger.logMessage("\(service) -> \(message)", console: false)
+        } else {
+            logMain(message: message)
+        }
     }
     
     public func logMain<T>(message : String = "", item: T...) where T : Encodable {
 //        let eventId = self.eventId
-        let logger = LogService(name: self.service, withStart: false)
         let encoder = JSONEncoder()
         if let data = try? encoder.encode(item), let parmsString = String(data: data, encoding:  .utf8) {
             logger.logMessage("\(service) ->\(message) - \(parmsString)", console: false)
@@ -69,8 +80,6 @@ public  struct EventManager {
         }
     }
     public func logMain(message : String) {
-//        let eventId =  self.eventId
-        let logger = LogService(name: self.service, withStart: false)
         logger.logMessage("\(service) -> \(message)", console: false)
     }
     
